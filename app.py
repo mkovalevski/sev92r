213e499adab9afd4d93066f1296c0969f7acef26
happy228 <kovalevski.m@yandex.ru>
2021-02-10 13:07:36 +0300

    some fixes

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
-from flask import Flask, render_template
-from data import departures, tours, title, subtitle, description, single_departures
 from random import randint
+
+from flask import Flask, render_template, abort
+
+from data import departures, tours, title, subtitle, description, single_departures
 
 app = Flask(__name__)
 app.jinja_env.add_extension('jinja2.ext.loopcontrols')
@@ -21,7 +23,6 @@ def index():
         else:
             b += 1
         i += 1
-    print(rand)
     for i in rand:
         new_tours[i] = tours[i]
     return render_template("index.html", title=title, tours=new_tours, departures=departures)
@@ -31,23 +32,32 @@ def index():
 def departure(departure):
     departure_prices = []
     departure_nights = []
-    for tour in tours:
-        if tours[tour]['departure'] == departure:
-            departure_prices.append(tours[tour]["price"])
-            departure_nights.append(tours[tour]["nights"])
-    return render_template('departure.html', departure=departure, tours=tours, departures=departures,
+    tours_for_departure = {}
+    for tour_id, tour in tours.items():
+        if tour['departure'] == departure:
+            departure_prices.append(tour['price'])
+            departure_nights.append(tour['nights'])
+            tours_for_departure[tour_id] = tour
+    return render_template('departure.html', departure=departure, tours=tours_for_departure, departures=departures,
                            title=title, single_departures=single_departures,
                            subtitle=subtitle, description=description,
                            departure_prices=departure_prices, departure_nights=departure_nights)
 
 
-@app.route('/tours/<id>/')
+@app.route('/tours/<int:id>/')
 def tour(id):
-    return render_template('tour.html', tours=tours, single_departures=single_departures, id=int(id),
-                           departures=departures, stars=int(tours[int(id)]["stars"]), title=title)
+    tour = tours.get(id)
+    if tour is None:
+        abort(404)
+    return render_template('tour.html',
+                           tour=tour,
+                           single_departures=single_departures,
+                           departures=departures,
+                           stars=int(tour["stars"]),
+                           title=title, )
 
 
-# app.run('localhost', port=8000, debug=True)
+app.run('localhost', port=8000, debug=True)
 
-if __name__ == '__main__':
-    app.run()
+# if __name__ == '__main__':
+#     app.run()
