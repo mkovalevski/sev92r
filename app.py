84b6a799d7233f32c724b6e20d25eaa7468f5fdf
km <kovalevski.m@yandex.ru>
2021-05-14 02:49:28 +0300

    some fixes

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template, abort
 
-from data import cars, title
+from data import cars, title, aparts, flat1
 
 app = Flask(__name__)
 
@@ -12,20 +12,30 @@ def index():
 
 @app.route('/about/')
 def about():
-    return render_template("about.html")
+    return render_template("about.html", title=title)
 
 
 @app.route('/terms/')
 def terms():
-    return render_template("terms.html")
+    return render_template("terms.html", title=title)
 
 
 @app.route('/apartments/')
 def apps():
-    return render_template("apps.html")
+    return render_template("apps.html", aparts=aparts, title=title)
 
 
-# app.run('localhost', port=8000, debug=True)
+@app.route('/flat1/')
+def app1():
+    return render_template("flat1.html", flat=flat1, title=title)
 
-if __name__ == '__main__':
-    app.run()
+
+@app.route('/flat2/')
+def app2():
+    return render_template("flat2.html", title=title)
+
+
+app.run('localhost', port=8000, debug=True)
+
+# if __name__ == '__main__':
+#     app.run()
